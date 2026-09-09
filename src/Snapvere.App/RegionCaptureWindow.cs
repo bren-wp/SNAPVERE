7dff7752a6b74df0bd9d032b9b9ca20e2fd8489d
Brendigo <info@brendigo.com>
2026-09-10 01:02:31 +0200

    Add WinUI automation namespace to Region editor

--- src/Snapvere.App/RegionCaptureWindow.cs
+++ src/Snapvere.App/RegionCaptureWindow.cs
@@ -1,6 +1,7 @@
 using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Automation;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
