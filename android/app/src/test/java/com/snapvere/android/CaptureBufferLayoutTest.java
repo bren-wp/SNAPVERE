44e75efe2f123dc5cda4c4b3f097ba885cf12542
Brendigo <info@brendigo.com>
2026-09-11 23:08:02 +0200

    Cover unexpected Android pixel stride

--- android/app/src/test/java/com/snapvere/android/CaptureBufferLayoutTest.java
+++ android/app/src/test/java/com/snapvere/android/CaptureBufferLayoutTest.java
@@ -20,6 +20,7 @@ public final class CaptureBufferLayoutTest {
     public void paddedWidthRejectsInvalidDimensionsAndStrides() {
         assertThrows(IllegalArgumentException.class, () -> CaptureBufferLayout.paddedWidth(0, 4, 4));
         assertThrows(IllegalArgumentException.class, () -> CaptureBufferLayout.paddedWidth(10, 0, 40));
+        assertThrows(IllegalArgumentException.class, () -> CaptureBufferLayout.paddedWidth(10, 8, 80));
         assertThrows(IllegalArgumentException.class, () -> CaptureBufferLayout.paddedWidth(10, 4, 0));
         assertThrows(IllegalArgumentException.class, () -> CaptureBufferLayout.paddedWidth(10, 4, 39));
     }
