51297009927e80dc95cb194f24da8727a0947b85
Brendigo <info@brendigo.com>
2026-09-22 11:07:35 +0200

    fix: import automation live-setting namespace

--- src/Snapvere.App/Services/OptionsWindow.cs
+++ src/Snapvere.App/Services/OptionsWindow.cs
@@ -1,6 +1,7 @@
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Automation;
+using Microsoft.UI.Xaml.Automation.Peers;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
